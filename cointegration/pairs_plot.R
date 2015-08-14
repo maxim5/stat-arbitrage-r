337b2c0a91fd3ce5de8417691c4a81acdb8326cf
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2015-08-14 17:31:35 +0200

    plot price

--- cointegration/pairs_plot.R
+++ cointegration/pairs_plot.R
@@ -6,6 +6,23 @@ suppressMessages(require(reshape2))
 
 load("pairs.RData")
 
+Plot.Price = function(symbol1, symbol2) {
+  series1 = all.logs[[symbol1]]
+  series2 = all.logs[[symbol2]]
+  dates = as.Date(rownames(all.logs))
+
+  data.to.plot = data.frame(exp(series1), exp(series2), dates)
+  colnames(data.to.plot) = c(symbol1, symbol2, "Date")
+  data.to.plot = melt(data.to.plot, id="Date")
+
+  plot = ggplot(data.to.plot, aes(x=Date, y=value, color=variable)) +
+    geom_line() +
+    labs(title=paste0(symbol1, " vs ", symbol2),
+         x="Time", y="Price") +
+    scale_colour_discrete(name="Legend")
+  print(plot)
+}
+
 Plot.Logs = function(symbol1, symbol2) {
   series1 = all.logs[[symbol1]]
   series2 = all.logs[[symbol2]]
@@ -47,6 +64,7 @@ Plot.Spread = function(spread, x=NULL, title="Spread") {
 }
 
 Plot.Pair = function(symbol1, symbol2) {
+  Plot.Price(symbol1, symbol2)
   Plot.Logs(symbol1, symbol2)
 
   series1 = all.logs[[symbol1]]
