ad00ee839d7c90c31e89990cfa51c2f832891413
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2015-08-21 18:17:11 +0200

    new exporter

--- cointegration/data_exporter.py
+++ cointegration/data_exporter.py
@@ -7,7 +7,7 @@ import csv
 import itertools
 
 
-MAX_PAIRS = 10
+MAX_PAIRS = 50
 
 
 def read_csv_rows(file_name, callback=lambda row: row):
@@ -17,20 +17,16 @@ def read_csv_rows(file_name, callback=lambda row: row):
 
 
 def export_to_py(data):
-    print "DATA = ["
-    print "  # symb1, symb2, coeff1, coeff2, spread mean, eps, delta, cost"
+    print "context.pairs = ("
     for row in data:
-        print "  ('%s', '%s', %2s, %2s, %s, %s, %s, %s)," % \
-              tuple([row[i] for i in [4, 5, 8, 7, 13, 2, 3, 10]])
-    print "]"
+        print "    Pair(symbols=symbols('%s', '%s'), gamma=%.6f, mean=%.6f, sd=%.6f, delta=%.6f, eps=0)," % \
+              tuple([row[i] for i in [0, 1]] + [float(row[i]) for i in [2, 5, 6, 6]])
+    print ")"
 
 
 def main():
-    pairs_data = read_csv_rows("result-tradable-pairs.csv", callback=lambda row: row[1:])
-    strategies = read_csv_rows("result-strategies.csv")
-    index = { ".".join(pair[0:2]): pair for pair in pairs_data }
-    merged = [strategy + index.get(strategy[0]) for strategy in itertools.islice(strategies, 0, MAX_PAIRS)]
-    export_to_py(merged)
+    pairs_data = read_csv_rows("result-cointegrated-pairs.csv", callback=lambda row: row[1:])
+    export_to_py(itertools.islice(pairs_data, 0, MAX_PAIRS))
 
 
 if __name__ == '__main__':
