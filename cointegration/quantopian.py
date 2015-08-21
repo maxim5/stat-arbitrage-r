3f540c471851c1fb0c67f3b6d292045ac99118dc
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2015-08-21 15:30:52 +0200

    rewrite algorithm

--- cointegration/quantopian.py
+++ cointegration/quantopian.py
@@ -6,6 +6,21 @@ import math
 import numpy
 
 
+def approx_rational(val, limit=25):
+    val, last_num, num = ((-val, -1, int(-val)) if val < 0 else (val, 1, int(val)))
+    last_den, den = 0, 1
+    rest, quot = val, int(val)
+    while abs(rest - quot) > 0.00001:
+        rest = 1.0 / (rest - quot)
+        quot = int(rest)
+        next_num = quot * num + last_num
+        next_den = quot * den + last_den
+        if abs(next_num) > limit or abs(next_den) > limit:
+            break
+        last_num, num, last_den, den = (num, next_num, den, next_den)
+    return num, den, val - float(num) / den
+
+
 OPEN = 0
 FILLED = 1
 CANCELLED = 2
@@ -16,12 +31,11 @@ UNWIND = 2
 
 
 class Pair:
-    def __init__(self, symbols, num1, num2, mean, eps, delta):
+    def __init__(self, symbols, gamma, mean, eps, delta):
         assert eps < delta
 
         self.symbol1, self.symbol2 = symbols
-        self.num1 = num1
-        self.num2 = num2
+        self.gamma = gamma
         self.mean = mean
         self.eps = eps
         self.delta = delta
@@ -36,24 +50,31 @@ class Pair:
 
 
     def spread(self, price1, price2):
-        return self.num1 * math.log(price1) - self.num2 * math.log(price2) - self.mean
+        return math.log(price1) - self.gamma * math.log(price2) - self.mean
 
 
-    def is_wide(self, spread):
+    def is_good_to_put_on(self, spread):
         return abs(spread) > self.delta
 
 
-    def is_narrow(self, spread):
-        return abs(spread) < self.eps or numpy.sign(spread) != self.direction
+    def is_good_to_unwind(self, spread):
+        return spread * self.direction < self.eps
 
 
-    def put_on(self, direction):
-        order_id1 = order_target(self.symbol1, -self.num1 * direction)
-        order_id2 = order_target(self.symbol2, self.num2 * direction)
+    def put_on(self, price1, price2):
+        share_ratio = self.gamma * price1 / price2
+        shares2, shares1, error = approx_rational(share_ratio, limit=50)
+
+        spread = self.spread(price1, price2)
+        direction = numpy.sign(spread)
+
+        order_id1 = order_target(self.symbol1, -shares1 * direction)
+        order_id2 = order_target(self.symbol2, shares2 * direction)
+        assert order_id1 and order_id2
+
         self.state = PUT_ON
         self.direction = direction
         self.orders = (order_id1, order_id2)
-        assert order_id1 and order_id2
 
 
     def unwind(self):
@@ -91,13 +112,9 @@ class Pair:
 
 def initialize(context):
     set_symbol_lookup_date("2015-01-01")
-    set_commission(commission.PerTrade(cost=0.03))
-    set_slippage(slippage.FixedSlippage(spread=0.01))
 
     context.pairs = (
-        Pair(symbols=symbols("VGIT", "SCHR"), num1=28, num2=29, mean=1.1836, eps=0.02, delta=0.06),
-        Pair(symbols=symbols("IEI", "ITE"), num1=19, num2=23, mean=-2.773, eps=0.02, delta=0.06),
-        Pair(symbols=symbols("QUAL", "IVV"), num1=5, num2=6, mean=-11.293, eps=0.02, delta=0.06),
+
     )
 
 
@@ -114,10 +131,10 @@ def handle_data(context, data):
 
             if order1.status == OPEN or order2.status == OPEN:
                 # Open orders. Check if the market went against us
-                if pair.state == PUT_ON and pair.is_narrow(spread):
+                if pair.state == PUT_ON and pair.is_good_to_unwind(spread):
                     pair.cancel(order1.status == OPEN, order2.status == OPEN)
                     vlog("CANCEL PUT ON", pair, context)
-                if pair.state == UNWIND and (pair.is_wide(spread) and numpy.sign(spread) == pair.direction):
+                if pair.state == UNWIND and (pair.is_good_to_put_on(spread) and numpy.sign(spread) == pair.direction):
                     vlog("bad unwind", pair, context)
 
             if order1.status == FILLED and order2.status == FILLED:
@@ -132,15 +149,18 @@ def handle_data(context, data):
 
         if not pair.orders:
             # Put on or unwind
-            if pair.state == READY and pair.is_wide(spread):
-                pair.put_on(direction=numpy.sign(spread))
+            if pair.state == READY and pair.is_good_to_put_on(spread):
+                pair.put_on(context.data1_.price, context.data2_.price)
                 vlog("PUT ON", pair, context)
-            elif pair.state == PUT_ON and pair.is_narrow(spread):
+            elif pair.state == PUT_ON and pair.is_good_to_unwind(spread):
                 pair.unwind()
                 vlog("UNWIND", pair, context)
 
         # Add a record
-        record(**{pair.name(): spread})
+        if len(context.pairs) < 5:
+            record(**{pair.name(): spread})
+        else:
+            record(leverage=context.account.leverage, total_positions_value=context.account.total_positions_value)
 
 
 def vlog(msg, pair, context):
